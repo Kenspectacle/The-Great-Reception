407d6760d6cacfe9382c7c866fcd3832b7e2d5f9
Kenspectacle <Kennyftw2@gmail.com>
2024-06-01 16:46:29 +0200

    feat: added a navbar

--- the-great-reception/src/app/page.tsx
+++ the-great-reception/src/app/page.tsx
@@ -1,10 +1,22 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Navbar from '../components/Navbar';
+import Script from 'next/script';  // Import Script from next/script
+import 'bootstrap/dist/css/bootstrap.css';
 
 export default function Home() {
   return (
-    <main>
-      <h1>The Great Reception</h1>
-    </main>
+    <>
+
+      <Navbar />
+      <main>
+        <h1>The Great Reception</h1>
+      </main>
+      <Script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" 
+      crossOrigin="anonymous"></Script>
+      <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" 
+      crossOrigin="anonymous"></Script>
+    </>
   );
 }
+
