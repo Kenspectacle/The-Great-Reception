985903024c763186fc2a86e2dbb9270b3de22a84
Kenspectacle <Kennyftw2@gmail.com>
2024-06-01 20:07:50 +0200

    feat: added an 'about' section

--- the-great-reception/src/app/page.tsx
+++ the-great-reception/src/app/page.tsx
@@ -1,9 +1,13 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Navbar from '../components/Navbar';
+import Footer from "../components/Footer";
 import Script from 'next/script';  // Import Script from next/script
-import 'bootstrap/dist/css/bootstrap.css';
+import Link from 'next/link';
+
 import './home.css'
+import 'bootstrap/dist/css/bootstrap.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 export default function Home() {
   return (
@@ -12,7 +16,7 @@ export default function Home() {
       <Navbar />
 
 
-      <header className="masthead bg-primary text-white text-center">
+      <header className="masthead bg-success text-white text-center">
         <div className="container d-flex align-items-center flex-column">
 
 
@@ -20,11 +24,7 @@ export default function Home() {
 
           <div className="divider-custom divider-light">
             <div className="divider-custom-line"></div>
-            <div className="divider-custom-line"></div>
             <div className="divider-custom-icon"><i className="fas fa-star"></i></div>
-            <div className="divider-custom-line"></div>
-            <div className="divider-custom-line"></div>
-            <div className="divider-custom-line"></div>
             <div className="divider-custom-line"></div>
           </div>
           <p className="masthead-subheading font-weight-light mb-0">An easy place to create a Recipe using AI</p>
@@ -33,8 +33,37 @@ export default function Home() {
 
 
 
+      <section className="page-section portfolio" id="portfolio">
+        <div className="container">
+          <h2 className="page-section-heading text-center text-uppercase text-secondary mb-0">
+            <Link href="/recipemaker">
+              <button className="btn btn-xl btn-outline-secondary">Click here to start!</button>
+            </Link>
+          </h2>
+          <div className="divider-custom">
+            <div className="divider-custom-line"></div>
+            <div className="divider-custom-icon"><i className="fas fa-star"></i></div>
+            <div className="divider-custom-line"></div>
+          </div>
+        </div>
+      </section>
 
+      <section className="page-section bg-primary text-white mb-0" id="about">
+            <div className="container">
+                <h2 className="page-section-heading text-center text-uppercase text-white">About</h2>
+                <div className="divider-custom divider-light">
+                    <div className="divider-custom-line"></div>
+                    <div className="divider-custom-icon"><i className="fas fa-star"></i></div>
+                    <div className="divider-custom-line"></div>
+                </div>
+                <div className="row">
+                    <div className="col-lg-4 ms-auto"><p className="lead">An AI recipe generator leverages advanced machine learning algorithms to create personalized and innovative recipes based on user preferences, dietary restrictions, and available ingredients. By analyzing vast amounts of culinary data, including cooking techniques, flavor pairings, and nutritional information, the AI can craft unique and delicious recipes tailored to individual tastes. </p></div>
+                    <div className="col-lg-4 me-auto"><p className="lead">This technology not only enhances culinary creativity but also simplifies meal planning, reduces food waste, and promotes healthier eating habits by suggesting balanced and diverse meal options.</p></div>
+                </div>
+            </div>
+        </section>
 
+      <Footer />
 
       <Script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
         crossOrigin="anonymous"></Script>
