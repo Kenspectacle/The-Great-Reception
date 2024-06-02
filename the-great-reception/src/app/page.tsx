a69e9cf121e3ffb689445cb2548395dce31aab67
Kenspectacle <Kennyftw2@gmail.com>
2024-06-02 17:03:04 +0200

    refactor: make redirect button its own component

--- the-great-reception/src/app/page.tsx
+++ the-great-reception/src/app/page.tsx
@@ -1,9 +1,6 @@
-import Image from "next/image";
-import styles from "./page.module.css";
 import Navbar from '../components/Navbar';
 import Footer from "../components/Footer";
-import Script from 'next/script';  // Import Script from next/script
-import Link from 'next/link';
+import RedirectButton from "../components/RedirectButton";
 
 import './home.css'
 import 'bootstrap/dist/css/bootstrap.css';
@@ -31,22 +28,7 @@ export default function Home() {
         </div>
       </header>
 
-
-
-      <section className="page-section portfolio" id="portfolio">
-        <div className="container">
-          <h2 className="page-section-heading text-center text-uppercase text-secondary mb-0">
-            <Link href="/recipemaker">
-              <button className="btn btn-xl btn-outline-secondary">Click here to start!</button>
-            </Link>
-          </h2>
-          <div className="divider-custom">
-            <div className="divider-custom-line"></div>
-            <div className="divider-custom-icon"><i className="fas fa-star"></i></div>
-            <div className="divider-custom-line"></div>
-          </div>
-        </div>
-      </section>
+      <RedirectButton buttonLink={"/recipemaker"} buttonDescription={"Click here to start!"}/>
 
       <section className="page-section bg-primary text-white mb-0" id="about">
             <div className="container">
@@ -64,11 +46,6 @@ export default function Home() {
         </section>
 
       <Footer />
-
-      <Script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
-        crossOrigin="anonymous"></Script>
-      <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy"
-        crossOrigin="anonymous"></Script>
     </>
   );
 }
